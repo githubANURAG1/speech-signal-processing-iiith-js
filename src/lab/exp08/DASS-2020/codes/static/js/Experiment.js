5e171c6f54bc0ae3640af18bd7b59145deef9f8d
Vipul Chhabra <vipulchhabra555@gmail.com>
2020-04-18 12:42:11 +0530

    Final Completion with Documentation

--- src/lab/exp08/DASS-2020/codes/static/js/Experiment.js
+++ src/lab/exp08/DASS-2020/codes/static/js/Experiment.js
@@ -1,3 +1,4 @@
+// This function defines the wavesurfer waveform and its related information
 var wavesurfer = WaveSurfer.create({
   container: "#waveform",
   waveColor: "#176696",
@@ -12,12 +13,15 @@ function clearcontent(element) {
   element.value = "";
 }
 
+// The function handling the play and pause button event
+
 function playButton() {
   wavesurfer.playPause();
 }
 
+// This functions handle the main container whenver any change is brought to the audio file option
 function generateSpectrum() {
-    document.getElementById("main-container").style.visibility = "visible";
+  document.getElementById("main-container").style.visibility = "visible";
   document.getElementById("windowformtype").selectedIndex = 0;
   changeSpectrum("rectangular");
   document.getElementById("nfftvalue").selectedIndex = 0;
@@ -26,64 +30,94 @@ function generateSpectrum() {
   changeorder(1);
 }
 
+// This function handles windowed waveform
 function changeSpectrum(elem) {
-    
-    var source = document.getElementById("windowedspectrums");
-    var clone = source.cloneNode(true);
-    clone.setAttribute('src','/graphs/window/line-'+document.getElementById("audionum").value+'-'+elem+'.html')
-    source.parentNode.replaceChild(clone,source);
+  var source = document.getElementById("windowedspectrums");
+  var clone = source.cloneNode(true);
+  clone.setAttribute(
+    "src",
+    "/graphs/window/line-" +
+      document.getElementById("audionum").value +
+      "-" +
+      elem +
+      ".html"
+  );
+  source.parentNode.replaceChild(clone, source);
 }
-
+// This functions handle Log spectrum
 function changefft(elem) {
-    var source = document.getElementById("nfftspectrum");
-    var clone = source.cloneNode(true);
-    clone.setAttribute('src','/graphs/logspectrum/stft-wav'+document.getElementById("audionum").value+'-nfft'+elem+'.html')
-    source.parentNode.replaceChild(clone,source);
+  var source = document.getElementById("nfftspectrum");
+  var clone = source.cloneNode(true);
+  clone.setAttribute(
+    "src",
+    "/graphs/logspectrum/stft-wav" +
+      document.getElementById("audionum").value +
+      "-nfft" +
+      elem +
+      ".html"
+  );
+  source.parentNode.replaceChild(clone, source);
 }
 
+// This function handles LP residual
 function changeorder(elem) {
-    
-    var source = document.getElementById("lpresidual");
-    var clone = source.cloneNode(true);
-    clone.setAttribute('src','/graphs/lpresidual/lpresidual-wav'+document.getElementById("audionum").value+'-order'+elem+'.html')
-    source.parentNode.replaceChild(clone,source);
+  var source = document.getElementById("lpresidual");
+  var clone = source.cloneNode(true);
+  clone.setAttribute(
+    "src",
+    "/graphs/lpresidual/lpresidual-wav" +
+      document.getElementById("audionum").value +
+      "-order" +
+      elem +
+      ".html"
+  );
+  source.parentNode.replaceChild(clone, source);
 
   generate_spectrum(elem);
 }
 
+// This function handles LP Spectrum
 function generate_spectrum(elem) {
-    var source = document.getElementById("lpspectrum");
-    var clone = source.cloneNode(true);
-    clone.setAttribute('src','/graphs/lpspectrum/lpspectrum-wav'+document.getElementById("audionum").value+'-order'+elem+'.html')
-    source.parentNode.replaceChild(clone,source);
+  var source = document.getElementById("lpspectrum");
+  var clone = source.cloneNode(true);
+  clone.setAttribute(
+    "src",
+    "/graphs/lpspectrum/lpspectrum-wav" +
+      document.getElementById("audionum").value +
+      "-order" +
+      elem +
+      ".html"
+  );
+  source.parentNode.replaceChild(clone, source);
 }
 
+// This function LOads audio on startup of the page.
 function LoadAudio(elem) {
   document.getElementById("audionum").value = elem.value;
   document.getElementById("windowformtype").selectedIndex = 0;
-    
-    document.getElementById("main-container").style.visibility = "hidden";
+
+  document.getElementById("main-container").style.visibility = "hidden";
 
   var source = document.getElementById("windowedspectrums");
-    var clone = source.cloneNode(true);
-    clone.setAttribute('src','')
-    source.parentNode.replaceChild(clone,source);
-    
-    var source = document.getElementById("nfftspectrum");
-    var clone = source.cloneNode(true);
-    clone.setAttribute('src','')
-    source.parentNode.replaceChild(clone,source);
-    
-    var source = document.getElementById("lpresidual");
-    var clone = source.cloneNode(true);
-    clone.setAttribute('src','')
-    source.parentNode.replaceChild(clone,source);
-    
-    var source = document.getElementById("lpspectrum");
-    var clone = source.cloneNode(true);
-    clone.setAttribute('src','')
-    source.parentNode.replaceChild(clone,source);
-    
+  var clone = source.cloneNode(true);
+  clone.setAttribute("src", "");
+  source.parentNode.replaceChild(clone, source);
+
+  var source = document.getElementById("nfftspectrum");
+  var clone = source.cloneNode(true);
+  clone.setAttribute("src", "");
+  source.parentNode.replaceChild(clone, source);
+
+  var source = document.getElementById("lpresidual");
+  var clone = source.cloneNode(true);
+  clone.setAttribute("src", "");
+  source.parentNode.replaceChild(clone, source);
+
+  var source = document.getElementById("lpspectrum");
+  var clone = source.cloneNode(true);
+  clone.setAttribute("src", "");
+  source.parentNode.replaceChild(clone, source);
+
   if (elem.value == 1 || elem.value == 2) {
     wavesurfer.load("/static/wav/audio" + elem.value + ".wav");
     console.log(elem.value);
---
+++ src/lab/exp08/DASS-2020/docs/Experiment.md
@@ -0,0 +1,11 @@
+EXPERIMENT 8
+
+1. The whole production ready code is availabe in codes folder.
+2. The flask part which was done before and other files are kept in archive folder.
+3. Also the plot geneartor to generate similar files has been ket in archive foler.
+4. In Experiment File the change has been made in the div with id = 'main-container'
+5. The CSS has not been changed
+6. All the graphs html pages are present in codes/graph folder with each type of grah kept in respective folder.
+7. The Linked JS is kept in static/js folder with File name Experiment.js
+
+The Documentation for Experiment.js is written in the file.
