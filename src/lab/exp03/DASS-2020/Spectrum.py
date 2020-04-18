2082e886b9c49f16e3f86b063994c467de59111d
Shubhi Dutta <shubhangi.dutta@research.iiit.ac.in>
2020-04-18 21:18:10 +0530

    final vowels

--- src/lab/exp03/DASS-2020/Spectrum.py
+++ src/lab/exp03/DASS-2020/Spectrum.py
@@ -30,39 +30,11 @@ def createSpectAutoCorr(audioFile):
     p.line(lpc_x, lpc)
     save(p, filename = 'graphs/lpc/'+audioFile+'lpc.html')
     
-    spf = wave.open("wav/"+audioFile+".wav", 'r') 
-    x = spf.readframes(-1)
-    x = numpy.fromstring(x, 'Int16')
-
-    N = len(x)
-    w = numpy.hamming(N)
-
-    x1 = x * w
-    x1 = lfilter([1], [1., 0.63], x1)
-
-    rts = numpy.roots(lpc)
-    rts = [r for r in rts if numpy.imag(r) >= 0]
-
-    angz = numpy.arctan2(numpy.imag(rts), numpy.real(rts))
-
-    Fs = spf.getframerate()
-    frqs = sorted(angz * (Fs / (2 * math.pi)))
-    fmin = 5
-    fmax= 20000
-    i_min = sr/fmax
-    i_max = sr/fmin
-    autoCorr[:int(i_min)] = 0
-    autoCorr[int(i_max):] = 0
-    i = autoCorr.argmax()
-    f0 = float(sr)/i
-    freqFile=open('freqFile.txt', "a")
-    freqFile.write(str(frqs)+" "+str(f0)+"\n")
-    freqFile.close()
-    return frqs, f0
+    
 
 
 
-os.remove('freqFile.txt')
-for i in range(1,5):
-    createSpectAutoCorr('ex'+str(i))
+for i in range(1,4):
+	for j in range(1,4):
+	    createSpectAutoCorr(str(i)+str(j))
 
