b0f6f75725459a4a13c3646f08678ff1fb3999f5
Shubhi Dutta <shubhangi.dutta@research.iiit.ac.in>
2020-04-20 21:15:37 +0530

    fully works

--- src/lab/exp03/DASS-2020/Experiment.js
+++ src/lab/exp03/DASS-2020/Experiment.js
@@ -1,60 +1,4 @@
-// This js file is for page 1 and 2
-/*
-// This function defines the wavesurfer waveform and its related information
-var wavesurfer = WaveSurfer.create({
-  container: "#waveform",
-  waveColor: "#176696",
-  barHeight: 2,
-  barGap: 1,
-  height: 400,
-  backgroundColor: "#f5f5f5",
-  normalize: "true",
-});
 
-// When page is loaded load timeline
-wavesurfer.on("ready", function () {
-  var timeline = Object.create(WaveSurfer.Timeline);
-
-  timeline.init({
-    wavesurfer: wavesurfer,
-    container: "#waveform-timeline",
-  });
-});
-
-// This function provides zoom for the experiment
-function zoom(elem) {
-  wavesurfer.zoom(elem.value);
-}
-
-// This function handles play button
-function playButton() {
-  wavesurfer.playPause();
-}
-
-
-
-
-function preload() {
-  var ctr=0
-  for (let i=0; i<3; i++){
-    for (let j=0; j<3; j++){
-      var contentval=[];
-     contentval[ctr] = loadStrings("wav/"+i+j+".txt");
-     ctr++;
-  }
-}
-  return contentval;
-
-}
-
-
-
-
-function setup() {
-  noCanvas();
-  //console.log(content1phn);
-}
-*/
 var content1, content2, val, fin;
 //This function changes content of the experiment on changing the experiment number
 function loadExpFrame() {
@@ -91,5 +35,59 @@ document.getElementById("main-container").style.visibility="visible";
 document.getElementById("autocorr").setAttribute("src", "graphs/autocorr/"+content1+content2+"autocorr.html")
 document.getElementById("lpc").setAttribute("src", "graphs/lpc/"+content1+content2+"lpc.html")
 console.log(document.getElementById("autocorr").getAttribute("src"))
+
 }
 
+function checkAns(){
+content1 = document.getElementById("speaker").value; 
+content2 = document.getElementById("vowel").value ;
+content1 = content1.toString()
+content2 = content2.toString()
+var expnum = content1+content2
+
+var client = new XMLHttpRequest();
+client.open('GET', 'wav/'+expnum+'.txt');
+client.onreadystatechange = function() {
+  window.fin = client.responseText;
+  fin = fin.split("\n")
+  f0 = parseInt(document.getElementById("F0").value)
+  f1 = parseInt(document.getElementById("F1").value)
+  f2 = parseInt(document.getElementById("F2").value)
+  f3 = parseInt(document.getElementById("F3").value)
+  for (i=0; i<4; i++){
+  	fin[i]=parseInt(fin[i])
+  }
+  if (fin[0]==f0){
+  	document.getElementById("f0a").innerHTML= "F0 is correct"
+}
+  else{
+  	var diff = f0-fin[0]
+  	document.getElementById("f0a").innerHTML= "F0 is wrong by " + diff + " values"
+  }
+  if (fin[1]==f1){
+  	document.getElementById("f1a").innerHTML= "F1 is correct"
+  }
+  else{
+  	var diff = f1-fin[1]
+  	document.getElementById("f1a").innerHTML= "F1 is wrong by " + diff + " values"
+  }
+  if (fin[2]==f2){
+  	document.getElementById("f2a").innerHTML= "F2 is correct"
+  }
+  else{
+  	var diff = f2-fin[2]
+  	document.getElementById("f2a").innerHTML= "F2 is wrong by " + diff + " values"
+  }
+  if (fin[3]==f3){
+  	document.getElementById("f3a").innerHTML= "F3 is correct"
+  }
+  else{
+  	var diff = f3-fin[3]
+  	document.getElementById("f3a").innerHTML= "F3 is wrong by " + diff + " values"
+  }
+
+}
+client.send();
+
+
+}
